136d4ed63db5f600754d7d0efd5e99dc737d1165
Finrod-Amandil <severin.zahler@gmail.com>
2017-08-17 09:40:42 +0200

    Initial commit

---
+++ JournalChecker/Person.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * PERSON.CS
+ * JournalChecker module
+ * 
+ * Module description:
+ * -------------------
+ * Person.cs contains the following functionalities:
+ * - Auxiliary class Person POJO
+ * 
+ */
+
+namespace JournalChecker
+{
+    class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string FullName { get; private set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            FullName = lastName + " " + firstName;
+        }
+    }
+}
